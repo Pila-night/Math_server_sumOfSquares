c68043175c2a86804ba1718809d7d9c743001f60
Pila-night <dbnz2005@list.ru>
2024-11-10 17:34:18 +0300

    Переделан метод чтения векторов

--- headers/Communicator.h
+++ headers/Communicator.h
@@ -12,10 +12,10 @@ class Communicator
 private:
     std::string dataFile;
     Logger& logger;
-    void readVector(std::vector<int32_t>& vectorValues);
+    std::vector<int32_t> readVector();
     int client_socket;
     void sendResponse(const std::string& response);
-
+    bool isValidInt32Vector(const std::vector<int32_t>& vectorValues);
 public:
     Communicator(int client_socket, const std::string& dataFile, Logger& logger);
 
