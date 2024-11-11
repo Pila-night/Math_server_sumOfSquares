e005e940eb6407950d4c107328bde967ac1c65f2
Pila-night <dbnz2005@list.ru>
2024-11-12 01:30:24 +0300

    Добавлен выброс исключения

--- ClientDataBase.cpp
+++ ClientDataBase.cpp
@@ -1,4 +1,5 @@
 #include "ClientDataBase.h"
+#include "Logger.h"
 using namespace std;
 ClientDataBase::ClientDataBase(const string& filename)
 {
@@ -19,14 +20,18 @@ void ClientDataBase::loadDatabase(const string& filename) {
             string password;
             getline(file, password); 
             password.erase(password.find_last_not_of(" \t\r\n") + 1);
-            database[login] = password;//тут пароль без знака переноса строки
-             database["2"] = "2";
+            database[login] = password; 
         }
+
         file.close();
     } else {
-        cerr << "Ошибка: Не удалось открыть файл " << filename << endl;
+         cerr << "Ошибка: Не удалось открыть файл " << filename << endl;
+         std::string error_message = strerror(errno);
+         std::string exception_message = "Критическая ошибка: Не удалось открыть файл " + filename + " " + error_message;
+         throw std::system_error(errno, std::generic_category(), exception_message);
     }
 }
+
 
 
 std::string& ClientDataBase::operator[](const std::string& login)
