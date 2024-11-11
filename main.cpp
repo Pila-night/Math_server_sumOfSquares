3d5fdff7180f4c987d306241f332d85ac7118f12
Pila-night <dbnz2005@list.ru>
2024-11-12 01:31:11 +0300

    Мейн теперь отвечает за запуск разбора ком строки и запуск сервера

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
-#include "Logger.h"
-#include "server.h"
+#include "StartInterface.h"
 
 #include <boost/program_options.hpp>
 #include <fstream>
@@ -8,60 +7,14 @@
 using namespace std;
 namespace po = boost::program_options;
 
-int main(int argc, char** argv)
-{
-    po::options_description desc("Options");
-    desc.add_options()
-        ("help,h", "Вывести справку")
-        ("data,d", po::value<string>()->default_value("/etc/vcalc.conf"),
-         "Имя файла с базой клиентов (необходимо запускать от имени администратора для доступа к этому файлу)")
-        ("log,l", po::value<string>()->default_value("/var/log/vcalc.log"),
-         "Имя файла с журналом работы (необходимо запускать от имени администратора для записи в этот файл)")
-        ("port,p", po::value<int>()->default_value(33333), "Номер порта для прослушки");
+    int main(int argc, const char** argv) {
+    try{
+    Interface UI;
+    UI.processCommands(argc, argv);
+    Server server(UI.getPort(), UI.getClientDataFile(), UI.getLogger());
+    server.start();
 
-    po::variables_map vm;
-
-    try {
-        if (argc == 1) {
-            cout << desc << endl;
-            return 0;
-        }
-        po::store(po::parse_command_line(argc, argv, desc), vm);
-        po::notify(vm);
-
-        if (vm.count("help")) {
-            cout << desc << endl;
-            return 0;
-        }
-
-        string clientDataFile = vm["data"].as<string>();
-        string logFile = vm["log"].as<string>();
-        int port = vm["port"].as<int>();
-
-        ifstream clientData(clientDataFile);
-        if (!clientData.is_open()) {
-            string error_message = "Файл с базой клиентов " + clientDataFile + " не найден.";
-            cerr << error_message << endl;
-            return 1; 
-        }
-
-        ofstream logFileStream(logFile, ios::app); 
-        if (!logFileStream.is_open()) {
-            string error_message = "Не удалось открыть файл журнала: " + logFile;
-            cerr << error_message << endl;
-            return 1; 
-        }
-
-        Logger logger(logFile); 
-
-        logger.log(INFO, "Файл с базой клиентов: " + clientDataFile);
-        logger.log(INFO, "Файл с журналом работы: " + logFile);
-        logger.log(INFO, "Порт: " + to_string(port));
-
-        Server server(port, clientDataFile, logger);
-        server.start(); 
-    } catch (const exception& e) {
-        cerr << "Ошибка: " << e.what() << endl; 
+    } catch (const exception& e) { 
         return 1; 
     }
 
